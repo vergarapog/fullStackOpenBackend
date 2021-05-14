cf8d92e9874a552abc7099b4af88464efab67ea6
vergarapog <201801166@iacademy.edu.ph>
2021-05-14 08:36:24 +0800

    finished 3.6

--- index.js
+++ index.js
@@ -41,6 +41,71 @@ let persons = [
       return response.json(persons)
   })
 
+  app.get('/api/persons/:id', (request, response) => {
+      const id = Number(request.params.id)
+      const person = persons.find( p => {
+          return p.id === id
+      })
+
+      if(person) {
+        return response.json(person)
+      } else {
+        console.log(`person`)
+        return response.status(404).end()
+      }
+  })
+
+  app.get('/info' , (request, response) => {
+      let num = persons.length
+      let time = new Date()
+      return response.send(`Phonebook has info for ${num} persons<br/><br/>
+      ${time}`)
+  })
+
+  app.delete('/api/persons/:id', (request,response) => {
+      const id = Number(request.params.id)
+      persons = persons.filter( note => note.id !== id)
+
+      response.status(204).end()
+  })
+
+  const generateId = () => {
+      const id = Math.floor(Math.random() * 10000) + 1;
+      return id
+  }
+
+  app.post('/api/persons', (request, response) => {
+      const body = request.body
+
+      if(!body.name || !body.number){
+        return response.status(400).json({
+          error: 'name or number is missing'
+        })
+      }
+      
+
+      const allNames = persons.map(p => p.name)
+      console.log(allNames)
+
+      if(allNames.includes(body.name)){
+        return response.status(400).json({
+          error: "name already exists"
+        })
+      }
+  
+      
+      
+      const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+      }
+
+      persons = persons.concat(person)
+      response.json(person)
+
+  })
+
   const PORT = 3001
   app.listen(PORT)
   console.log(`app running on ${PORT}`)
