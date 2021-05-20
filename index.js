ee7106dabc0e03813126c02d1aefa8cc8c8c2704
vergarapog <201801166@iacademy.edu.ph>
2021-05-20 18:11:30 +0800

    finished 3.11

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 app.use(cors());
+app.use(express.static("build"));
 
 morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
